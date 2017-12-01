a1b9d9ddb635e03a6d85f5c131d1a6b0d917165d
marcocastignoli <marco.castignoli@gmail.com>
2017-12-01 12:27:20 +0100

    Added mocha and chai tests

---
+++ test/distributed-database-explorer.test.js
@@ -0,0 +1,27 @@
+var Entity = require("../build/distributed-database-explorer.js").Entity;
+var expect = require("chai").expect;
+
+
+describe('distributed-database-explorer', function () {
+
+    it('resolves', (done) => {
+        let a = new Entity("4", "database")
+        const result = a.get()
+            .then((database) => {
+                return database.findBySlug("nationalities")
+            })
+            .then((nationalities) => {
+                return nationalities.findBySlug("italian")
+            })
+            .then((italian) => {
+                return italian.findBySlug("tofur", false)
+            })
+            .then((tofur) => {
+                return tofur.get("name")
+            })
+            
+        result.then((name) => {
+            expect(name).to.equal('Tofur');
+        }).then(done, done);
+    });
+});
